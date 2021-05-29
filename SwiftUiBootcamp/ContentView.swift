b2fbdef568cc7678d1030cbfba4248d7b0d55945
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-05-29 21:52:31 +0530

    Initial Commit

---
+++ SwiftUiBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 29/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiBootcamp/SwiftUiBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBootcampApp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
