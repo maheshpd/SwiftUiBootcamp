ed55c859f46bb2d8a97d6d0ca8d68f41a7689820
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-05-30 21:21:44 +0530

    How to use System Icons, Multi-Color Icons, and SF Symbols

---
+++ SwiftUiBootcamp/IconsBootcamp.swift
@@ -0,0 +1,32 @@
+//
+//  IconsBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 30/05/21.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+        Image(systemName: "person.fill.badge.plus")
+            .renderingMode(.original)
+            .font(.largeTitle)
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .scaledToFill()
+//            .font(.caption)
+//            .font(.system(size: 200))
+//            .foregroundColor(Color(#colorLiteral(red: 0.1450980392, green: 0.6117647059, blue: 0.2078431373, alpha: 1)))
+//            .frame(width: 300, height: 300)
+//            .clipped()
+        
+        
+    }
+}
+
+struct IconsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        IconsBootcamp()
+    }
+}
