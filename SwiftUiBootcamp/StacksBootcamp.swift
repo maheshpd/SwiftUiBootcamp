f5b16bdaffdd2ed41c87189ebdf3985aa3c5d96c
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-06-04 08:16:17 +0530

    How to use VStack, HStack, and ZStack

---
+++ SwiftUiBootcamp/StacksBootcamp.swift
@@ -0,0 +1,54 @@
+//
+//  StacksBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 04/06/21.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    //VStacks -> Vertical
+    //HStack -> Horizontal
+    //ZStack -> zIndex (back to front)
+    var body: some View {
+        ZStack(alignment: .top) {
+            Rectangle()
+                .fill(Color.yellow)
+                .frame(width: 350, height: 500, alignment: .center)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 150, height: 150)
+                
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: 100, height: 100)
+                
+                HStack(alignment: .bottom){
+                    
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 50, height: 50)
+                    
+                    Rectangle()
+                        .fill(Color.pink)
+                        .frame(width: 75, height: 75)
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 25, height: 25)
+                    
+                }.background(Color.white)
+                
+            }.background(Color.black)
+        }
+    }
+}
+
+struct StacksBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StacksBootcamp()
+    }
+}
