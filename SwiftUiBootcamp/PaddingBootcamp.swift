cbd5db6960041833ed11ab03819ff48b4ef614f1
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-06-06 07:37:26 +0530

    How to use Padding

---
+++ SwiftUiBootcamp/PaddingBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  PaddingBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 06/06/21.
+//
+
+import SwiftUI
+
+struct PaddingBootcamp: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+            Text("This is the description of what we will do on this screen. It is multiple lines and we will align the text to the leading edge.")
+        }
+        .padding()
+        .padding(.vertical, 30)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 10)
+        )
+        .padding(.horizontal, 10)
+    }
+}
+
+struct PaddingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PaddingBootcamp()
+    }
+}
