6ac1993d6e97881d850acd8c95bfe0028a02855a
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-08-01 13:14:31 +0530

    ToggleBootcamp

---
+++ SwiftUiBootcamp/ToggleBootcamp.swift
@@ -0,0 +1,42 @@
+//
+//  ToggleBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 01/08/21.
+//
+
+import SwiftUI
+
+struct ToggleBootcamp: View {
+    
+    @State var toggleIsOn: Bool = false
+    
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                
+                Text("Status:")
+                Text(toggleIsOn ? "Online" : "Offline")
+                
+            }
+            
+            Toggle(
+                isOn: $toggleIsOn,
+                label: {
+                Text("Change Status")
+            })
+                .toggleStyle(SwitchToggleStyle(tint: Color(#colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1))))
+            
+            Spacer()
+        }
+        .padding(.horizontal, 100)
+    }
+}
+
+struct ToggleBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleBootcamp()
+    }
+}
