cabc18ad7407c4df5472dc6f9e6423c94f428a23
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-08-03 09:21:50 +0530

    How to use DatePicker (and DatePickerStyle)

---
+++ SwiftUiBootcamp/DatePickerBootcamp.swift
@@ -0,0 +1,48 @@
+//
+//  DatePickerBootcamp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Mahesh Prasad on 02/08/21.
+//
+
+import SwiftUI
+
+struct DatePickerBootcamp: View {
+    
+    @State var selectedDate: Date = Date()
+    let startingDate: Date = Calendar.current.date(from: DateComponents(year: 2018)) ?? Date()
+    let endingDate: Date = Date()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
+    var body: some View {
+        
+        VStack{
+            Text("SELECTED DATE IS:")
+            Text(dateFormatter.string(from: selectedDate))
+                .font(.title)
+            
+    //        DatePicker("Select a Date", selection: $selectedDate)
+    //        DatePicker("Select a date", selection: $selectedDate, displayedComponents: [.date, .hourAndMinute])
+            DatePicker("Select a date", selection: $selectedDate, in: startingDate...endingDate)
+                .accentColor(Color.red)
+                .datePickerStyle(
+                    CompactDatePickerStyle()
+    //                GraphicalDatePickerStyle()
+    //                WheelDatePickerStyle()
+                )
+        }
+        
+    }
+}
+
+struct DatePickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerBootcamp()
+    }
+}
